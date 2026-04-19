54a4882f6d6bbc8c86716878f70ddeb9dfde1324
Gunth3rr <gunther.sotelo@pregrado.uoh.cl>
2026-04-18 23:09:00 -0400

    Update db.sql
    rut primary key

--- db.sql
+++ db.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
-  `rut` int DEFAULT NULL,
+  `rut` varchar PRIMARY KEY,
   `nombre` varchar(30) DEFAULT NULL,
   `apellido` varchar(30) DEFAULT NULL,
   `rol` char(10) DEFAULT NULL
