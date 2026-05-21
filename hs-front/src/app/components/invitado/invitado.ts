a60c96689cd27c4a3d614de86e551014515089e8
Claudio <claudio.parra@pregrado.uoh.cl>
2026-05-21 18:27:36 -0400

    eliminar archivos antiguos

--- hs-front/src/app/components/invitado/invitado.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-invitado',
-  imports: [],
-  templateUrl: './invitado.html',
-  styleUrl: './invitado.css',
-})
-export class Invitado {}
