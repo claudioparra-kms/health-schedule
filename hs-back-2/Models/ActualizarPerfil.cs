873f5931827749aa2afa6a4199842ccc9fa5c9d7
Gunther Sotelo <gunther.sotelo@pregrado.uoh.cl>
2026-06-16 23:31:24 -0400

    Modificar Datos de Usuario

---
+++ hs-back-2/Models/ActualizarPerfil.cs
@@ -0,0 +1,12 @@
+namespace proyecto_ids_api.Models
+{
+    public class ActualizarPerfilModel
+    {
+        public int UsuarioId { get; set; }
+        public string Correo { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Direccion { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
