1492a254557d4516356b289b3f9251b910620f36
claudio <claudio.parra@pregrado.uoh.cl>
2026-07-07 17:19:21 -0400

    workflow

--- hs-front-2/src/app/dashboard-paciente/dashboard-paciente.spec.ts
+++ hs-front-2/src/app/dashboard-paciente/dashboard-paciente.spec.ts
@@ -1,1 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DashboardPaciente } from './dashboard-paciente';
+
+describe('DashboardPaciente', () => {
+  let component: DashboardPaciente;
+  let fixture: ComponentFixture<DashboardPaciente>;
+  let compiled: HTMLElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DashboardPaciente]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DashboardPaciente);
+    component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
